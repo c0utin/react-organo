579c205c82891281604ffa659bea704694d2fecc
rafmontenegro <rafaelcouto111@gmail.com>
2023-08-02 15:04:44 -0300

    feat: hide empty teams

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -4,12 +4,13 @@ import './Team.css'
 
 const Team = (props) => {
   return (
-    <section className='team'style={{ backgroundColor: props.colorSecundary }}>
+    (props.employees.length > 0) ? <section className='team'style={{ backgroundColor: props.colorSecundary }}>
       <h3 style={{ borderColor: props.colorPrimary  }}>{props.name}</h3>
       <div className='employees'>
         {props.employees.map( employee => <Employeer name={employee.name} office={employee.office} image={employee.image} /> )}
       </div>
     </section>
+    : ''
   )
 }
 
