38ea625534439b3560c7a8cb5b73bfcc6174b3ab
rafmontenegro <rafaelcouto111@gmail.com>
2023-08-21 18:56:26 -0300

    fix: image path directory

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,9 +1,10 @@
 import './Banner.css';
+import banner from '../../assets/banner.png'
 
 const Banner = () => {
   return (
     <header className="banner">
-       <img src="/images/banner.png" alt="Principle banner of Organo"/>
+      <img src={banner} alt="Principle banner of Organo" />
     </header>
   );
 };
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,8 @@
 import './Footer.css'
+import facebook from '../../assets/fb.png'
+import instagram from '../../assets/ig.png'
+import twitter from '../../assets/tw.png'
+import logo from '../../assets/logo.png'
 
 const Footer = () => {
     return (<footer className="footer">
@@ -6,23 +10,23 @@ const Footer = () => {
             <ul>
                 <li>
                     <a href="facebook.com" target="_blank">
-                        <img src="/images/fb.png" alt="" />
+                            <img src={facebook} alt="" />
                     </a>
                 </li>
                 <li>
                     <a href="twitter.com" target="_blank">
-                        <img src="/images/tw.png" alt="" />
+                        <img src={twitter} alt="" />
                     </a>
                 </li>
                 <li>
                     <a href="instagram.com" target="_blank">
-                        <img src="/images/ig.png" alt="" />
+                        <img src={instagram} alt="" />
                     </a>
                 </li>
             </ul>
         </section>
         <section>
-            <img src="/images/logo.png" alt="" />
+            <img src={logo} alt="" />
         </section>
         <section>
             <p>
